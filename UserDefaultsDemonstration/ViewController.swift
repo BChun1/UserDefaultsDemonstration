dce2cd400f193b188eadc29558fe48c6ef961137
BChun1 <BChun37@yahoo.com>
2017-04-26 05:46:24 -0700

    Initial Commit

---
+++ UserDefaultsDemonstration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserDefaultsDemonstration
+//
+//  Created by Brian Chun on 4/26/17.
+//  Copyright © 2017 Brian Chun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
